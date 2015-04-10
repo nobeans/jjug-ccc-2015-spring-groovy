7333210acae9bead6d7c9f406e0bec4f7f037c40
Yasuharu Nakano <ynak@jggug.org>
2015-04-10 18:41:12 +0900

    Complete database samples

--- database/dataSet.groovy
+++ database/dataSet.groovy
@@ -1,12 +1,9 @@
-
 @Grab('com.h2database:h2')
-@GrabConfig(systemClassLoader=true) // JDBCはシステムクラスローダから探されるため必要
-
+@GrabConfig(systemClassLoader=true) // JDBCはシステムクラスローダから探されるので必要
 import groovy.sql.Sql
 
 // 別途DataSourceやConnectionを用意するなら、Sqlのコンストラクタに渡せばOK
-def db = Sql.newInstance("jdbc:h2:mem:", "org.h2.Driver")
-
+def db = Sql.newInstance("jdbc:h2:mem:sample", "org.h2.Driver")
 
 // Sql#execute()でDDLを実行する
 db.execute("""
@@ -24,8 +21,9 @@ people.add(name: 'Mike', age:13)
 people.add(name: 'Junko', age: 14)
 people.add(name: 'Ken', age: 23)
 
-people.findAll { it.age < 20 }.sort { it.age }.each {
+people.each {
     println "${it.name} (${it.age})"
 }
-
-
+//=> Mike (13)
+//   Junko (14)
+//   Ken (23)
